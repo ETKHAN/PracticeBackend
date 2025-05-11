c509029516cc678eaf49aa8b2d5f9a713dc847b9
etizaz <etizaazkhaan@gmail.com>
2025-05-11 07:41:34 +0500

    Cors and register user added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
 
+app.use(cors({
+  origin : process.env.CORS_ORIGIN,
+  credentials: true
+}));
 // middlewares ignored
+
+
 
 import userRouter from "./routes/user.routes.js";
 app.use('/api/v1/users', userRouter)
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,21 +7,26 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 const registerUser = asyncHandler(async (req , res) => {
+  
   const {username, email, fullname, password} = req.body
   if([fullname, email, username, password].some(field => field?.trim() === "")){
     throw new ApiError(400, "All fields Are compulsory")
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or : [{ username }, { email }]
   })
 
   if(existedUser) throw new ApiError(409, "User with email or username already exist");
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if(req.files.coverImage)
+    coverImageLocalPath = req.files.coverImage[0].path;
 
   if(!avatarLocalPath) throw new ApiError(400, "Avatar file is required!");
+
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
@@ -41,11 +46,22 @@ const registerUser = asyncHandler(async (req , res) => {
     "-password -refreshToken"
   )
 
+
   if(!isUserCreated) throw new ApiError(500, "Something went wrong while registering user");
 
-  return res.status(201).json(
-    new ApiResponse(200, isUserCreated, "user registerd Successfully")
-  );
+
+
+  try {
+    return res.status(201).json(
+      new ApiResponse(201, isUserCreated, "User registered successfully")
+    );
+  } catch (error) {
+    console.error("User creation failed:", error);
+    return res.status(500).json(
+      new ApiResponse(500, null, "Internal Server Error")
+    );
+  }
+  
 
 
 });
